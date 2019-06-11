e1dc7f5ec51e648f050790b2c3c6e28aabfbc511
Rodrigo Guerrero <warrior.software.minds@gmail.com>
2019-06-11 12:58:34 -0500

    initial commit

---
+++ app/src/main/java/com/warriorminds/countries/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.warriorminds.countries
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
